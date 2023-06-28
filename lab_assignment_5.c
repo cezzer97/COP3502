e49c06890b36fd1b814a753cf2f70a599989849b
cezzer97 <cezzer1997@gmail.com>
2023-06-28 09:59:26 -0400

    void ExchangeCharacters(char str[], int i, int j) {     char temp = str[i];     str[i] = str[j];     str[j] = temp; }
    void RecursivePermute(char str[], int k) {
        int j;
        if (k == strlen(str)) {
            printf("%s\n", str);
        } else {
            for (j = k; j < strlen(str); j++) {
                ExchangeCharacters(str, k, j);
                RecursivePermute(str, k + 1);
                ExchangeCharacters(str, j, k);
            }
        }
    }
    
    void Permute(char str[]) {
        RecursivePermute(str, 0);
    }
    
    int main() {
        char str[] = "abc";
        Permute(str);
        return 0;
    }

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -1,91 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-typedef struct node {
-    char letter;
-    struct node* next;
-} node;
-
-// Returns the number of nodes in the linked list.
-int length(node* head) {
-    int count = 0;
-    node* current = head;
-    while (current != NULL) {
-        count++;
-        current = current->next;
-    }
-    return count;
+void ExchangeCharacters(char str[], int i, int j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
 }
 
-// Converts the linked list to a C-style string.
-// Example: if the linked list is head -> |a|->|b|->|c|,
-// then toCString function will return "abc".
-char* toCString(node* head) {
-    int len = length(head);
-    char* str = (char*)malloc((len + 1) * sizeof(char));
-    node* current = head;
-    int i = 0;
-    while (current != NULL) {
-        str[i++] = current->letter;
-        current = current->next;
-    }
-    str[i] = '\0';  // Add the null terminator
-    return str;
-}
-
-// Inserts a character at the end of the linked list.
-// If the linked list is head -> |a|->|b|->|c|,
-// then insertChar(&head, 'x') will update the linked list as follows:
-// head -> |a|->|b|->|c|->|x|.
-void insertChar(node** pHead, char c) {
-    node* newNode = (node*)malloc(sizeof(node));
-    newNode->letter = c;
-    newNode->next = NULL;
-
-    if (*pHead == NULL) {
-        *pHead = newNode;
+void RecursivePermute(char str[], int k) {
+    int j;
+    if (k == strlen(str)) {
+        printf("%s\n", str);
     } else {
-        node* current = *pHead;
-        while (current->next != NULL) {
-            current = current->next;
+        for (j = k; j < strlen(str); j++) {
+            ExchangeCharacters(str, k, j);
+            RecursivePermute(str, k + 1);
+            ExchangeCharacters(str, j, k);
         }
-        current->next = newNode;
     }
 }
 
-// Deletes all nodes in the linked list.
-void deleteList(node** pHead) {
-    node* current = *pHead;
-    while (current != NULL) {
-        node* temp = current;
-        current = current->next;
-        free(temp);
-    }
-    *pHead = NULL;
+void Permute(char str[]) {
+    RecursivePermute(str, 0);
 }
 
-int main(void) {
-    int i, strLen, numInputs;
-    node* head = NULL;
-    char* str;
-    char c;
-    FILE* inFile = fopen("input.txt", "r");
-    fscanf(inFile, "%d", &numInputs);
-    while (numInputs-- > 0) {
-        fscanf(inFile, "%d", &strLen);
-        for (i = 0; i < strLen; i++) {
-            fscanf(inFile, " %c", &c);
-            insertChar(&head, c);
-        }
-        str = toCString(head);
-        printf("String is: %s\n", str);
-        free(str);
-        deleteList(&head);
-        if (head != NULL) {
-            printf("deleteList is not correct!");
-            break;
-        }
-    }
-    fclose(inFile);
+int main() {
+    char str[] = "abc";
+    Permute(str);
     return 0;
 }
